3101506f00d2fc41539e405efb4f9d02fd092932
Agustin Carmusciano <agustincarmu99@gmail.com>
2023-05-22 20:47:05 -0300

    Trabajo finalizado x2

--- TPE/Parte1/Principal.java
+++ TPE/Parte1/Principal.java
@@ -14,13 +14,7 @@ public class Principal {
         grafo.agregarVertice(6);
         grafo.agregarVertice(7);
         grafo.agregarVertice(8);
-        // grafo.agregarVertice(9);
-        // grafo.agregarVertice(10);
-        // grafo.agregarVertice(11);
-        // grafo.agregarVertice(12);
-        // grafo.agregarVertice(13);
-        // grafo.agregarVertice(14);
-        // grafo.agregarVertice(15);
+
 
         grafo.agregarArco(1, 2, 10);
         grafo.agregarArco(1, 3, 20);
@@ -39,49 +33,15 @@ public class Principal {
 
 
 
-        // grafo.agregarArco(1, 2, 10);
-        // grafo.agregarArco(1, 3, 20);
-        // grafo.agregarArco(1, 5, 30);
-        // grafo.agregarArco(2, 3, 40);
-        // grafo.agregarArco(2, 6, 50);
-        // grafo.agregarArco(6, 12, 60);
-        // grafo.agregarArco(12, 2, 70);
-        // grafo.agregarArco(3, 4, 80);
-        // grafo.agregarArco(3, 8, 90);
-        // grafo.agregarArco(4, 9, 100);
-        // grafo.agregarArco(4, 8, 110);
-        // grafo.agregarArco(5, 11, 120);
-        // grafo.agregarArco(5, 7, 130);
-        // grafo.agregarArco(7, 6, 140);
-        // grafo.agregarArco(6, 13, 150);
-        // grafo.agregarArco(11, 10, 160);
-        // grafo.agregarArco(14, 11, 170);
-        // grafo.agregarArco(15, 14, 180);
-        // grafo.agregarArco(13, 15, 190);
-
-
         ServicioDFS dfs = new ServicioDFS(grafo); 
         ServicioBFS bfs = new ServicioBFS(grafo);
 
         // System.out.println(dfs.dfsForest().toString());
+        // System.out.println(bfs.bfsForest().toString());
 
         ServicioCaminos servicioCaminos = new ServicioCaminos(grafo, 2, 5, 6);
         // System.out.println(servicioCaminos.caminos().toString());
 
-        // Iterator<Integer> vertices = grafo.obtenerVertices();
-        // Iterator<Arco<Integer>> arcos = grafo.obtenerArcos();
-        
-        // while(arcos.hasNext()){
-            // System.out.println(arcos.next());
-            // }
-            // while(vertices.hasNext()){
-                //     System.out.println(vertices.next());
-                // }
-                
-
-                grafo.borrarVertice(5);
-                grafo.borrarArco(4, 1);
-                System.out.println(grafo.cantidadArcos());
-            }
-        }
+    }
+}
         
